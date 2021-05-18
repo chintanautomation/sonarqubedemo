a147d4c659368d2b88f7f03c5f3d69bc3c1fd174
chintanautomation <chintanchavda1991@gmail.com>
2021-05-18 16:56:46 +0530

    First Commit

--- src/main/java/com/chintan/SonarQubeDemo/SonarQubeDemoApplication.java
+++ src/main/java/com/chintan/SonarQubeDemo/SonarQubeDemoApplication.java
@@ -8,6 +8,7 @@ public class SonarQubeDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SonarQubeDemoApplication.class, args);
+		System.out.println("Welcome");
 	}
 
 }
